2db1be97901ea5e709ee36a49ce7d3be6ba73ab8
divyasen <divyas@vt.edu>
2017-04-24 03:18:54 -0400

    Added intial to mass

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 var bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -24,19 +25,20 @@ app.get('/', function (req, res) {
 app.get('/search', function(req ,res) {
 var foodSubGroup = req.query.foodGroup;
 var mass = req.query.mass;
-mass = mass /100;
+mass = 2.7 * 7 * mass /100;
 
 connection.query('SELECT avg(unit_price_g_avg) AS price FROM price WHERE fsg_id=' + foodSubGroup, function(err, rows) {
 	if (err){
 		throw err;
 	}
-
-	res.send(rows[0]);
-
-/*	res.render(__dirname + "/views/stat",
+	console.log(rows[0].price);
+//	res.send(rows[0]);
+	res.render(__dirname + "/views/stat",
 		rows[0]
 		
-	)*/
+		
+	)
+
 })
 
 })

76d8e818d71ad03fcf7baf773ca397161f579db7
Ubuntu <ubuntu@ip-172-31-3-11.ec2.internal>
2017-04-28 19:41:07 +0000

    Added documentation for the webserver

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//Adde all necessary rquires
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+//Middleware for parsing get and post requests
 var bodyParser = require('body-parser');
+//We are using ejs as our expression languge, so we set that here
 app.set('view engine', 'ejs');
+//Connect to the db, make sure these configs align with what is set
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -11,46 +15,50 @@ var connection = mysql.createConnection({
   port : '3306'
 });
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
+//This is where we store all filos being sent to the public
 app.use(express.static('public'));
-
+//This is where we connect to the db
 connection.connect();
 
 
-
+//Serve the index page
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/views/index.html" );
 })
 
-
+//Serve the sources page
 app.get('/sources', function (req, res) {
    res.sendFile( __dirname + "/views/sources.html" );
 })
 
-
+//Serve the about page
 app.get('/about', function (req, res) {
    res.sendFile( __dirname + "/views/about.html" );
 })
 
 app.get('/search', function(req ,res) {
+//Grab the JSON object that was passed in the client
 var foodSubGroup = JSON.parse(req.query.foodGroup);
 var fsgList = [];
 var mass = 0;
 var data = {};
+//Create a list of all the FSG igs
 for( var ind in foodSubGroup){
 	fsgList.push(ind);
 }
 
-
+//Query for all the IDS to recieve the price its associated with
 connection.query('SELECT fsg_id, avg(unit_price_g_avg) AS price FROM price GROUP BY fsg_id HAVING fsg_id IN (' + fsgList.toString() + ");", function(err, rows) {
 	if (err){
 		throw err;
 	}
-	
+	//This is where we calculate the combined mass of the food
 	for(var ind in foodSubGroup){
 		mass += parseFloat(foodSubGroup[ind]);
 	}
+	//Multiply by 2.5, to signify one meal waste extrapolated to an entire day
 	mass *= 2.5;
+	//We multipplu by the decimal because we are converting it to pounds
 	data.day = Math.round(mass * .00220462);
 	mass *= 7;
 	data.week = Math.round(mass * .00220462);
@@ -59,8 +67,10 @@ connection.query('SELECT fsg_id, avg(unit_price_g_avg) AS price FROM price GROUP
 	mass *= 12;
 	data.year = Math.round(mass * .00220462); 
 	data.price = 0;
+	//We multiply the mass by the price here
 	for(var ind in rows){
 		var temp = rows[ind]	
+		//We divbide by 100, because the database is price  per 100 grams
 		data.price += parseFloat(foodSubGroup[temp["fsg_id"]]) * parseFloat(temp["price"])/100;
 	
 	}
